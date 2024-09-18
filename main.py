e495b6078c1665ca5f8ef36484800e8a7d1f1e49
asterrie <155606475+asterrie@users.noreply.github.com>
2024-09-18 19:55:16 +0200

    main.py
     chyba to bedzie bazowy plik

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import sys
+import os
 
+os.environ["OPENAI_API_KEY"] = ""
