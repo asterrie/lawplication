18e23aab1466b72354a7bcafff5ca6d89930c8af
asterrie <155606475+asterrie@users.noreply.github.com>
2024-10-04 19:58:18 +0200

    Update main.py
    more lines added (I have to reinstall the vstudiooo)

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from llama_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain import OpenAI
 import sys
 import os
 
 os.environ["OPENAI_API_KEY"] = ""
+
+def creativeVectorIndex(path):
+    max_input = 4096
+    tokens = 256
+    chunk_size = 600
+    max_chunk_overlap = 20
+
+    prompt_helper = PromptHelper(max_input, tokens, max_chunk_overlap, chunk_size_limit=chunk_size )
